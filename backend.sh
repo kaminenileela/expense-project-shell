a4b382b359de236539887ca398dd288c7b73693b
Leela Kamineni <kaminenileela1992@gmail.com>
2024-04-30 17:31:34 -0400

    backend

---
+++ backend.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+
+if [ $1 -ne 0 ]
+then
+    echo -e "$2...$R FAILURE $N"
+    exit 1
+else
+    echo -e "$2...$G SUCCESS $N"
+fi
+
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run script with super user access"
+    exit 2
+else
+    echo "You are super user"
+fi
+
+dnf module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOGFILE
+VALIDATE $? "Enabling nodejs version 20"
+
+dnf install nodejs -y &>>$LOGFILE
+VALIDATE $? "Installing nodejs"
+
+id expense &>>$LOGFILE
+if [ $? -ne 0 ]
+then
+useradd expense &>>$LOGFILE
+VALIDATE $? "creating expense user"
+else
+ echo -e "Expense user already created ... $Y SKIPPING $N"
+
+mkdir -p /app 
+VALIDATE $? "creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip 
+VALIDATE $? "downloading backend code"
+
+cd /app
+
+unzip /tmp/backend.zip 
+VALIDATE $? "Extracted backend code"
+
+npm install 
+VALIDATE $? "installing nodejs dependencies"
+
+
+
+
+
+
+
