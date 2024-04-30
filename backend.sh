715f746875910493eeb170ab44ccf1aa9b9e83c5
Leela Kamineni <kaminenileela1992@gmail.com>
2024-04-30 17:42:01 -0400

    backend

--- backend.sh
+++ backend.sh
@@ -9,6 +9,9 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
+echo "Enter Password:"
+read -s PASSWORD
+
 VALIDATE(){
 
 if [ $1 -ne 0 ]
@@ -46,21 +49,39 @@ VALIDATE $? "creating expense user"
 else
  echo -e "Expense user already created ... $Y SKIPPING $N"
 
-mkdir -p /app 
+mkdir -p /app &>>$LOGFILE
 VALIDATE $? "creating app directory"
 
-curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip 
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip &>>$LOGFILE
 VALIDATE $? "downloading backend code"
 
 cd /app
 
-unzip /tmp/backend.zip 
+unzip /tmp/backend.zip &>>$LOGFILE
 VALIDATE $? "Extracted backend code"
 
-npm install 
+npm install &>>$LOGFILE
 VALIDATE $? "installing nodejs dependencies"
 
+cp /home/ec2-user/expense-project-shell/backend.service /etc/systemd/system/backend.service
+VALIDATE $? "copied backend service"
 
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "Daemon Reload"
+systemctl start backend &>>$LOGFILE
+systemctl enable backend &>>$LOGFILE
+VALIDATE $? "Starting and Enabling backend"
+
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing mysql client"
+
+
+mysql -h db.learningdevopsaws.online -uroot -p$PASSWORD < /app/schema/backend.sql &>>$LOGFILE
+VALIDATE $? "Schema loading"
+
+
+systemctl restart backend &>>$LOGFILE
+VALIDATE $? "restaring backend"
 
 
 
